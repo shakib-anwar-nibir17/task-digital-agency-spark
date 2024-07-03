6840929b4b8732f733f9309aea943259172a3f3b
shakib-anwar-nibir17 <nibirshakib@gmail.com>
2024-07-03 22:54:15 +0600

    ant design error

--- app/layout.js
+++ app/layout.js
@@ -1,3 +1,4 @@
+import { AntdRegistry } from "@ant-design/nextjs-registry";
 import { Inter } from "next/font/google";
 import "./globals.css";
 
@@ -11,7 +12,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <AntdRegistry>{children}</AntdRegistry>
+      </body>
     </html>
   );
 }
